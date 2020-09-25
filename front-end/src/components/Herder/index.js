393ef0343d279e5415259f56d0b6ffb5a7925387
wyao <289672494@qq.com>
2020-09-25 15:11:48 +0800

    [wang yao] 添加antd前端框架 完成herder

---
+++ front-end/src/components/Herder/index.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import { Menu } from 'antd';
+import { MailOutlined } from '@ant-design/icons';
+
+
+class Header extends Component {
+  state = {
+    current: 'store',
+  };
+
+  handleClick = e => {
+    this.setState({ current: e.key });
+  };
+
+  render() {
+    const { current } = this.state;
+    return (
+      <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key="store" icon={<MailOutlined />}>
+          商城
+        </Menu.Item>
+        <Menu.Item key="order" icon={<MailOutlined />}>
+          订单
+        </Menu.Item>
+        <Menu.Item key="goodsAdd" icon={<MailOutlined />}>
+          添加商品
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+export default Header;
