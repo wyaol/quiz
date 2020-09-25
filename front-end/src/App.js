b51850383b93b3a88b41832fd6264365abfa6430
wyao <289672494@qq.com>
2020-09-25 18:10:21 +0800

    [wang yao] 显示订单列表

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Header from './components/Herder';
 import GoodsAdd from './components/GoodsAdd';
 import GoodsList from './components/GoodsList';
+import OrdersList from './components/OrdersList';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route path="/goods/add" component={GoodsAdd}/>
         <Route path="/goods" component={GoodsList}/>
+        <Route path="/orders" component={OrdersList}/>
       </Switch>
     </Router>
   );
--- front-end/src/components/GoodsList/index.js
+++ front-end/src/components/GoodsList/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import './index.css';
-import Header from '../Herder';
 
 
 class GoodsList extends Component {
