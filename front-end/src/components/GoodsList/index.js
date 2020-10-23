73132da08ed302797cd71a0e5ad3bd5a3d91ca2f
wyao <289672494@qq.com>
2020-10-23 09:44:20 +0800

    [wang yao] feature:购物车前端组件

--- front-end/src/components/GoodsList/index.js
+++ front-end/src/components/GoodsList/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import { Popover, Button, Table, Space, InputNumber } from 'antd';
 import './index.css';
 
 
@@ -26,16 +27,60 @@ class GoodsList extends Component {
   }
 
   render() {
+    const columns = [
+      {
+        title: '商品',
+        dataIndex: 'goods',
+        key: 'goods',
+        render: text => <a>{text}</a>,
+      },
+      {
+        title: '数量',
+        dataIndex: 'number',
+        key: 'number',
+        render: number => <InputNumber min={1} max={10} defaultValue={1} />
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <Space size="middle">
+            <a>Delete</a>
+          </Space>
+        ),
+      },
+    ];
+    
+    const data = [
+      {
+        key: '1',
+        goods: 'John Brown',
+        number: 32,
+      }
+    ];
+
+    const content = (
+      <div>
+        <Table columns={columns} dataSource={data} />
+        <button>立即下单</button>
+        <button>清空</button>
+      </div>
+    );
+
     return (
       <div>
         {this.state.goods.map((good, index) => (
-          <div className="good">
+          <div className="good" key={index}>
             <img src={good.imgUrl} alt=""/>
             <span>{good.name}</span>
             <span>单价{good.price}元/{good.unit}</span>
             <button onClick={() => this.addOrder(good.id)}>+</button>
           </div>
         ))}
+
+        <Popover content={content} title="Title">
+          <Button type="primary">Hover me</Button>
+        </Popover>
       </div>
     );
   }
